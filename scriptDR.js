515817b4dd569f246ada306943e14f5562cdb67d
Agirre Xabat Maortua (CSC E IN) <XabatMaortua.Agirre@infineon.com>
2025-03-18 10:48:46 +0100

    Full screen WebOWL visualization

--- scriptDR.js
+++ scriptDR.js
@@ -1,8 +1,18 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const links = document.querySelectorAll('.map-container .idc');
+    const fullScreenBtn = document.getElementById('fullScreenBtn');
+    const fullContainer = document.getElementById('fullScreenContainer');
+    const closeBtn = document.getElementById('closeBtn');
+
+    fullScreenBtn.addEventListener('click', () => {
+        fullContainer.style.display = "block";
+    });
+    closeBtn.addEventListener('click', () => {
+        fullContainer.style.display = "none";
+    });
 
     links.forEach(link => {
-        link.addEventListener('mouseenter', function() {
+        link.addEventListener('mouseenter', function () {
             const mapId = this.dataset.mapId;
             const mapArea = document.getElementById(mapId);
             if (mapArea) {
@@ -10,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
 
-        link.addEventListener('mouseleave', function() {
+        link.addEventListener('mouseleave', function () {
             const mapId = this.dataset.mapId;
             const mapArea = document.getElementById(mapId);
             if (mapArea) {
