5736555c8f084a71c617ea8322b2387ff40fe5b4
Maortua Agirre Xabat (CSC E IN / External) <maortuaagirre.external@infineon.com>
2026-03-13 10:47:51 +0100

    WebVOWL visualization loaded after website is fully loaded

--- scriptDR.js
+++ scriptDR.js
@@ -1,9 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
+    const mainIframe = document.getElementById('mainIframe');
     const fullScreenBtn = document.getElementById('fullScreenBtn');
     const fullContainer = document.getElementById('fullScreenContainer');
     const closeBtn = document.getElementById('closeBtn');
     const fullScreenIframe = document.getElementById('fullScreenIframe');
     const loadingIndicator = document.getElementById('loadingIndicator');
+
+    setTimeout(() => {
+        if (mainIframe && !mainIframe.src) {
+            mainIframe.src = "https://ifx-dr.github.io/WebVOWL/#digitalreference";
+        }
+    }, 100);
 
     fullScreenBtn.addEventListener('click', () => {
         fullContainer.style.display = "block";
